4dd4cf3f4a9ac98ae3208195ba06dea3deb5468e
Witchapat Issariyawat <witchapat.issariyawat@ibm.com>
2022-10-15 17:23:05 +0700

    add style to navigation bar

--- src/components/navigation-bar-component.jsx
+++ src/components/navigation-bar-component.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-
+import './navigation-bar-component.css';
 const Navigation = ({ resume } ) => {
     console.log(resume);
     return (
@@ -41,47 +41,13 @@ const Navigation = ({ resume } ) => {
 
 
       </header> */}
-
-      <header id="home">
-         <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-            <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
-            </ul>
-         </nav>
-
-         <div className="row banner">
-            <div className="banner-text">
-               <h1 className="responsive-headline">I am {resume.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resume.role}.{resume.roleDescription}
-               </h3>
-               <hr/>
-               <ul className="social">
-                  {
-                    resume.socialLinks && resume.socialLinks.map(item =>{
-                      return(
-                              <li key={item.name}>
-                                <a href={item.url} target="_blank"><i className={item.className}></i></a>
-                              </li>
-                            )
-                          }
-                    )
-                  }
-               </ul>
+        <div className='menu__container'>
+            <div className='menu'>
+                <Link to='/'>Main</Link>
+                <Link to='/home'>Home</Link>
             </div>
-         </div>
-
-         <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-         </p>
-
-      </header>
+        </div>
+        <Outlet></Outlet>
         </Fragment>
     )
 }
